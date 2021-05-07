692369ed2667d292553e857c379757dc1434d0c2
Michael Herr <michaelherr@Michaels-MacBook-Pro-2.local>
2021-05-06 23:11:57 -0400

    Connect Four Grid Created

---
+++ connectfour-app/app.js
@@ -0,0 +1,9 @@
+const gameContainer = document.querySelector('.game-container');
+
+
+// for(let i = 0; i < 42; i++){
+//     const square = document.createElement('div');
+//     square.classList.add('block');
+//     gameContainer.append(square);
+// }
+
