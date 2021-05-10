ac3c61ee51ffccf909e050f39b0c8905814121e3
Michael Herr <michaelherr@Michaels-MacBook-Pro-2.local>
2021-05-10 12:18:52 -0400

    updated

--- connectfour-app/app.js
+++ connectfour-app/app.js
@@ -1,9 +1,30 @@
-const gameContainer = document.querySelector('.game-container');
+// Have two colors, one red and one yell.
+//Have red go first and pick 1 position out of the 42 positions in the container.
+//Yellow will then go next and select the any position that red has not already picked.
+//Each color will continue to take turns selecting an open position. 
+//Once one color has consecutively connected four positions either horizontially, vertically, or diagonally, that color wins.
 
 
-// for(let i = 0; i < 42; i++){
-//     const square = document.createElement('div');
-//     square.classList.add('block');
-//     gameContainer.append(square);
-// }
+const container = document.querySelector('.game-contaier');
 
+
+const playerOne = [];
+const playerTwo = [];
+
+
+const selectPosition = (e)  => {
+    
+    if (e.currentTarget.style.backgroundColor === 'red' || e.currentTarget.style.backgroundColor === 'yellow'){
+        return;
+    }else if(playerOne.length === playerTwo.length){
+        e.currentTarget.style.backgroundColor = 'red';
+        playerOne.push(e);
+    }else if (playerOne.length > playerTwo.length){
+        e.currentTarget.style.backgroundColor ='yellow';
+        playerTwo.push(e);
+    } 
+}
+
+document.querySelectorAll('.block').forEach((block) => {
+    block.addEventListener('click', selectPosition); 
+}); 
