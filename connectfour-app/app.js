57bd648541c519c2723592fc666d60d81c4930a5
Michael Herr <michaelherr@Michaels-MacBook-Pro-2.local>
2021-05-11 00:59:16 -0400

    Finding a new way to change the colors

--- connectfour-app/app.js
+++ connectfour-app/app.js
@@ -1,4 +1,4 @@
-// Have two colors, one red and one yell.
+// Have two colors, one red and one yellow.
 //Have red go first and pick 1 position out of the 42 positions in the container.
 //Yellow will then go next and select the any position that red has not already picked.
 //Each color will continue to take turns selecting an open position. 
@@ -6,25 +6,97 @@
 
 
 const container = document.querySelector('.game-contaier');
+const cells = document.querySelectorAll('.block');
+let bottom = document.querySelectorAll('.bottom');
+const currentPlayer = document.querySelector('#currentPlayer')
+const playerOne = 'red'
+const playerTwo = 'yellow'
+let player1 = true;
 
+// for (let i = 0; i < cells.length; i++){
+//     console.log(i);
+// }
 
-const playerOne = [];
-const playerTwo = [];
-
-
-const selectPosition = (e)  => {
+const changeColor = (event) => {
+    const divSelected = event.currentTarget;
+    if(player1){
+        divSelected.style.backgroundColor = playerOne;
+        player1 = false;
+    }else if(!player1){
+        divSelected.style.backgroundColor = playerTwo;
+        player1 = true;
+    }
     
-    if (e.currentTarget.style.backgroundColor === 'red' || e.currentTarget.style.backgroundColor === 'yellow'){
-        return;
-    }else if(playerOne.length === playerTwo.length){
-        e.currentTarget.style.backgroundColor = 'red';
-        playerOne.push(e);
-    }else if (playerOne.length > playerTwo.length){
-        e.currentTarget.style.backgroundColor ='yellow';
-        playerTwo.push(e);
-    } 
 }
 
-document.querySelectorAll('.block').forEach((block) => {
-    block.addEventListener('click', selectPosition); 
+
+
+
+
+
+cells.forEach((cell) => {
+    cell.addEventListener('click', changeColor); 
 }); 
+
+
+// if (e.currentTarget.style.backgroundColor === 'red' || e.currentTarget.style.backgroundColor === 'yellow'){
+//     return;
+// }else if(player = 1){
+//     e.currentTarget.style.backgroundColor = 'red';
+//     e.currentTarget.classList.add('bottom');
+//     player = 2;
+//     currentPlayer.innerText = player;
+// }else if (player = 2){
+//     e.currentTarget.style.backgroundColor ='yellow';
+//     player = 1;
+//     currentPlayer.innerText = player;
+// }
+
+// const playerOne = [];
+// const playerTwo = [];
+// const selectPosition = (e)  => {
+//     let player = 1;
+
+//     if (e.currentTarget.style.backgroundColor === 'red' || e.currentTarget.style.backgroundColor === 'yellow'){
+//         return;
+//     }else if(playerOne.length === playerTwo.length){
+//         e.currentTarget.style.backgroundColor = 'red';
+//         e.currentTarget.classList.add('full');
+//         playerOne.push(e);
+//         player = 2;
+//         currentPlayer.innerText = player;
+//     }else if (playerOne.length > playerTwo.length){
+//         e.currentTarget.style.backgroundColor ='yellow';
+//         playerTwo.push(e);
+//         player = 1;
+//         currentPlayer.innerText = player;
+//     }
+    
+// }; 
+
+
+
+
+// const playerTurn = (e) => {
+//     if(player = 1){ 
+//         e.currentTarget.style.backgroundColor = 'red';
+//         player = 2;
+//         currentPlayer.innerText = player;
+//     }else if( player = 2){
+//          e.currentTarget.style.backgroundColor = 'yellow';
+//         player = 1;
+//         currentPlayer.innerText = player;
+//     };
+    
+// };
+
+
+
+// const columnOne = document.querySelectorAll('.column1')
+//     const checkColumnOne = (e) => {
+//         color = e.currentTarget.style.backgroundColor
+//         console.log(color);
+//     }
+    // document.querySelectorAll('.column1').forEach((column) => {
+    //     column.addEventListener('click', checkColumnOne)
+    // });
